fb32631b9dd409ac0358a3b31428887dfa622a7d
BrahySebastien <b.seb.b@hotmail.com>
2018-05-13 12:58:42 +0200

    image foond

--- src/main/java/viewPackage/PanneauBienvenue.java
+++ src/main/java/viewPackage/PanneauBienvenue.java
@@ -1,25 +1,42 @@
 package viewPackage;
 
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
+import java.io.File;
+import java.io.IOException;
 
 public class PanneauBienvenue extends JPanel
 {
+    private JPanel panelImage;
     private JLabel bienvenue;
     private GifAnimationThread gifAnimationThread;
+    private Image imgFond;
 
     public PanneauBienvenue ()
     {
+        panelImage = new JPanel();
+        try {
+            imgFond = ImageIO.read(new File("fond.jpg"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         //Layout + ajout d'une police d'écriture via le label
         this.setLayout(new BorderLayout());
         bienvenue = new JLabel("Bienvenue !");
         bienvenue.setFont(new Font("Helvetica", Font.ITALIC, 100));
         bienvenue.setHorizontalAlignment(0);
-        this.setBackground(Color.RED);
-        this.add(bienvenue, BorderLayout.NORTH);
+
+
+        this.add(bienvenue,BorderLayout.NORTH);
+        this.add(panelImage,BorderLayout.CENTER);
+
 
         //Test d'affichage du gif
         gifAnimationThread = new GifAnimationThread(this);
         gifAnimationThread.start();
+    }
+    public void paintComponent(Graphics g){
+        g.drawImage(imgFond,0,0,PanneauBienvenue.this);
     }
 }
